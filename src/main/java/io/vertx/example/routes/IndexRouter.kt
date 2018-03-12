e3c23641bb038cdfb9dc55c9e3c5e3b7b1924e73
Gary Lo <gary.lo@harborx.com>
2018-03-12 14:12:29 +0800

    switch to new KRouter ext method

--- src/main/java/io/vertx/example/routes/IndexRouter.kt
+++ src/main/java/io/vertx/example/routes/IndexRouter.kt
@@ -3,22 +3,21 @@ package io.vertx.example.routes
 import io.vertx.example.foundation.KRouter
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
-import java.util.concurrent.CompletableFuture
 import kotlin.coroutines.experimental.suspendCoroutine
 
 class IndexRouter : KRouter() {
     init {
-        get("/").handleCoroutine { req ->
-            req.response().end("OK")
+        get("/").handleCoroutine { _ ,res ->
+            res.send("OK")
         }
-        get("/2ms").handleCoroutine { req ->
-            req.response().end(fakeIOOperation(2))
+        get("/2ms").handleCoroutine { _ ,res ->
+            res.send(fakeIOOperation(2))
         }
-        get("/5ms").handleCoroutine { req ->
-            req.response().end(fakeIOOperation(5))
+        get("/5ms").handleCoroutine { _ ,res ->
+            res.send(fakeIOOperation(5))
         }
-        get("/10ms").handleCoroutine { req ->
-            req.response().end(fakeIOOperation(10))
+        get("/10ms").handleCoroutine { _ ,res ->
+            res.send(fakeIOOperation(10))
         }
     }
 
